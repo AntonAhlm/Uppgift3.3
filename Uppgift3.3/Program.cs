ef454c3ff53bb976118f3630ce61d19e11c336fc
Anton <>
2023-02-10 08:45:50 +0100

    Add project files.

---
+++ Uppgift3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra en bil?");
+            int tid = Convert.ToInt32(Console.ReadLine());
+            int pris = tid * 80 ;
+
+            if (pris > 950)
+                pris = 950;
+
+            Console.WriteLine("Det kommer att kosta " + pris + "kr.");
+
+ 
+        }
+    }
+}
